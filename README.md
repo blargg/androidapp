23adc22b5b0b04323315ad22bc0f18c08f03fe0d
blargg <anuibis@sbcglobal.net>
2012-11-26 22:57:12 -0600

    comments

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-There's not much here right now. Haven't had much time to work on it yet :(
+Android App
+===========
+
+Hey!!! if your reading this, i think we can help keep track of things here
+
+What Works
+----------
+* Linear Picture View -- displays images and text in a scrollable view (dynamic)
+
+What We Still Need
+------------------
+* Parsing the json
+* Downlading Images / Mapping Images
--- src/com/example/my/first/app/LinearPictures.java
+++ src/com/example/my/first/app/LinearPictures.java
@@ -17,35 +17,41 @@ public class LinearPictures extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
+		
+		// the main scrolling view of this activity
 		HorizontalScrollView sv = new HorizontalScrollView(this);
 		
+		// organizes all the different image text pairs horizontally
 		LinearLayout ll = new LinearLayout(this);
 		ll.setOrientation(LinearLayout.HORIZONTAL);
-		ll.setGravity(Gravity.CENTER);
+		ll.setGravity(Gravity.CENTER); // center all the objects (cause those are views... this may not matter)
 		sv.addView(ll);
 		
 		for(int i = 0; i < 20; i++){
+			// holds corresponding pairs of image and text (vertically)
 			LinearLayout tmp = new LinearLayout(this);
 			tmp.setOrientation(LinearLayout.VERTICAL);
-			tmp.setGravity(Gravity.CENTER);
+			tmp.setGravity(Gravity.CENTER); // center the text and the images
 			tmp.setPadding(10, 10, 10, 10);
 			
+			// make a new image to add
 			ImageView imgV = new ImageView(this);
 			imgV.setImageResource(R.drawable.ic_launcher);
 			tmp.addView(imgV);	
 			
+			// make text to add
 			TextView txt = new TextView(this);
 			txt.setText(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
 			tmp.addView(txt);
 			
-			ll.addView(tmp);
+			ll.addView(tmp); // add the small view to the big one
 		}
 
 		
-		Button b = new Button(this);
+		Button b = new Button(this); // here just because
 		b.setText("Test Text");
 		ll.addView(b);
-		setContentView(sv);
+		setContentView(sv); // set the view to the main view
 	}
 
 	@Override
